bd10e670faae70d3abf292bf02b6b5ff77461ee9
pietrobe03 <19685185+pietrobe03@users.noreply.github.com>
2019-01-01 15:09:27 +0100

    Added comments

--- ArduinoLib/randuinoconnect.h
+++ ArduinoLib/randuinoconnect.h
@@ -10,33 +10,38 @@ class RanduinoConnect : public QObject
     Q_OBJECT
 
 public:
+    // Constructor, Destructor
     explicit RanduinoConnect(QObject *parent = nullptr);
     ~RanduinoConnect();
 
+    // Options for event triggers [More will be added soon]
     enum Trigger {DISCONNECT};
 
+    // Public methods for randuino communication
     bool isConnected();
     bool openConnection(QString port);
     void closeConnection();
     void setSlots(Trigger t, QObject *o, const char* s);
-
     QString SendCommand(QString command, int timeout = 2);
 
-
 private slots:
+    
     QString SerialWaitForLine(int timeout = 2);
+    
+    // Periodically called methods
     void ReadSerial();
     void CheckConnection();
 
 private:
-    bool connected;
-    bool alreadyRead;
-    QByteArray read;
-    QTimer *ti;
-    QSerialPort *sp;
+    bool connected;     // Flag showing if a connection should currently be established
+    bool alreadyRead;   // Flag showing if the contents stored in read are new or already read
+    QByteArray read;    // Incoming data until last terminating character ('\n')
+    QTimer *ti;         // Timer for periodical actions
+    QSerialPort *sp;    // Handle for serial port connection
 
 signals:
+    // Event signals
     void ConnectionLost();
 };
 
-#endif // ARDUINOCONNECT_H
+#endif // RANDUINOCONNECT_H
