9b80402ef29e5bdc1d1f753b52a8632a196bf49f
pietrobe03 <19685185+pietrobe03@users.noreply.github.com>
2019-03-22 21:37:40 +0100

    Update randuino-value.ino

--- arduino-scripts/randuino-value/randuino-value.ino
+++ arduino-scripts/randuino-value/randuino-value.ino
@@ -1,15 +1,11 @@
-#include <MD5.h>
-/*
- * TO USE THIS, THE MD5-LIBRARY FOR ARDUINO IS NEEDED:
- * https://github.com/tzikis/ArduinoMD5
- */
 #include <QueueList.h>
 #include <string.h>
 
 // Definition of output pins
-int led1Pin = 13;
-int led2Pin = 12;
-int led3Pin = 11;
+int beeper = A1;
+int greenLED = 7;
+int redLED = 8;
+int blueLED = 9;
 
 // Definition of input pin
 int inpPin = A0;
@@ -26,16 +22,15 @@ void setup() {
   Serial.setTimeout(100);
 
   // Initialization of pins
-  pinMode(led1Pin, OUTPUT);
-  pinMode(led2Pin, OUTPUT);
-  pinMode(led3Pin, OUTPUT);
+  pinMode(beeper, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  pinMode(redLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
   pinMode(inpPin, INPUT);
 }
 
 // Periodically running code
 void loop() {
-
-  static String salt = "00";
   
   // Parse serial commands
   if (Serial.available() > 0) {
@@ -43,47 +38,41 @@ void loop() {
 
     // Command parser ("command" => answer)
     if (input.equals("single")) { // "single" => Random number
-
-        // Put together the analog value and the salt
-        char* temp;
-        sprintf(temp, "%i%s", analogRead(inpPin), salt.c_str());
-
-        // Hash it
-        unsigned char* hash = MD5::make_hash(temp);
-        char* md5chr = MD5::make_digest(hash, 16);
         
-        // Extract new salt and random number
-        String rng = String(md5chr[0]) + String(md5chr[1]);
-        salt = String(md5chr[random(0,15)]) + String(md5chr[random(0,15)]); // Pseudo-random numbers create additional randomness for the salt
+        analogWrite(beeper, 128);
 
-        // Convert number from hex to dec and send it to the client 
-        Serial.print(strtol(rng.c_str(), NULL, 16) + "\n");
-
-        // Free some memory
-        free(temp);
-        free(md5chr);
-        free(hash);
-        
-        // Flash green led
-        digitalWrite(led3Pin, HIGH);
-        delay(50);
-        digitalWrite(led3Pin, LOW);
-      
+        int num = 0;
+        for (int i = 0; i < 8; i++) {
+          int t = 0;
+          for (int j = 0; j < 8; j++) {
+            delay(2);
+            t += analogRead(inpPin);
+          }
+          num = num * 2 + (t % 2);
+        }
+        Serial.print((String) num + "\n");
+        analogWrite(beeper, 0);// Flash green led
+        digitalWrite(greenLED, HIGH);
+        delay(15);
+        digitalWrite(greenLED, LOW);
     } else if (input.equals("connect")) { // "connect" => "randuino"
         Serial.print("randuino\n");
 
-        // Flash red led
-        digitalWrite(led1Pin, HIGH);
+        // Flash blue led
+        digitalWrite(blueLED, HIGH);
         delay(50);
-        digitalWrite(led1Pin, LOW);
+        digitalWrite(blueLED, LOW);
         
     } else if (input.equals("heartbeat")) { // "heartbeat" => "still_alive"
         Serial.print("still_alive\n");
 
     } else {
       Serial.print("CMD_ERR: " + input + "\n");
+      // Flash red led
+      digitalWrite(redLED, HIGH);
+      delay(50);
+      digitalWrite(redLED, LOW);
       
     }
-  } 
-
+  }
 }
